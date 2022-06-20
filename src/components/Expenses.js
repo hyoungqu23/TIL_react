e50d8d2e1120edcff8fbbfe64f5613f2f369c5bc
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-06-21 03:19:00 +0900

    [feat] Expenses Component
    styled-component 추가

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,8 +1,9 @@
 import ExpenseItem from './ExpenseItem';
+import styled from 'styled-components';
 
 const Expenses = ({ expenses }) => {
   return (
-    <div>
+    <StyledExpenses>
       {expenses.map((expense) => {
         return (
           <ExpenseItem
@@ -12,8 +13,18 @@ const Expenses = ({ expenses }) => {
           />
         );
       })}
-    </div>
+    </StyledExpenses>
   );
 };
 
 export default Expenses;
+
+const StyledExpenses = styled.div`
+  padding: 1rem;
+  background-color: rgb(31, 31, 31);
+  margin: 2rem auto;
+  width: 50rem;
+  max-width: 95%;
+  border-radius: 12px;
+  box-shadow: 0 1px 8px rgba(0, 0, 0, 0.25);
+`;
