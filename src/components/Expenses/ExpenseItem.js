e044849aae7db71dc7df919ae056549f7bf54081
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-06-22 08:39:32 +0900

    [nit] expenseItem Component
    불필요한 코드 제거

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,20 +4,12 @@ import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const handleButtonClick = () => {
-    setTitle('Clicked!');
-    console.log(title);
-  };
-
   return (
     <StyledExpenseItem className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
-        <button onClick={handleButtonClick}>Change Title</button>
       </div>
     </StyledExpenseItem>
   );
