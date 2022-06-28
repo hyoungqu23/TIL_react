0d9e1ceaf546c335479b169e7472b304e3b51609
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-06-28 21:24:37 +0900

    [nit] semantic tag 설정

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,18 @@
-import { useState } from 'react';
 import styled from 'styled-components';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
   return (
-    <StyledExpenseItem className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">{props.amount}</div>
-      </div>
-    </StyledExpenseItem>
+    <li>
+      <StyledExpenseItem className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">{props.amount}</div>
+        </div>
+      </StyledExpenseItem>
+    </li>
   );
 };
 
